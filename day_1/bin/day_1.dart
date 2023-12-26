6a992ed12e3e517d959ea9e5b2876eb3f20cd3fc
Wesley Cheek <66931618+wcheek@users.noreply.github.com>
2023-12-26 09:52:10 +0900

    Day one.. but doesn't work

--- day_1/bin/day_1.dart
+++ day_1/bin/day_1.dart
@@ -1,5 +1,11 @@
+import 'dart:io';
 import 'package:day_1/day_1.dart' as day_1;
+import 'package:collection/collection.dart';
 
-void main() {
-  print('Hello world: ${day_1.calculate()}!');
+void main() async {
+  File input = File("lib/input.txt");
+  List<String> contents = await input.readAsLines();
+  final codedInts = day_1.getCodedInts(contents);
+  contents.asMap().forEach((ind, val) => print("$val: ${codedInts[ind]}"));
+  print(codedInts.sum);
 }
--- day_1/lib/day_1.dart
+++ day_1/lib/day_1.dart
@@ -1,3 +1,39 @@
-int calculate() {
-  return 6 * 7;
+enum WordNums {
+  one,
+  two,
+  three,
+  four,
+  five,
+  six,
+  seven,
+  eight,
+  nine,
+}
+
+String numeralMatch = r"(\d)";
+String wordMatch = r"(one|two|three|four|five|six|seven|eight|nine)";
+RegExp regex = RegExp("$numeralMatch|$wordMatch");
+
+Map<String, String> getWordToNumeralMap() {
+  Map<String, String> wordToNumeral = {};
+  WordNums.values.asMap().forEach(
+      (index, wordNum) => wordToNumeral[wordNum.name] = "${index + 1}");
+  return wordToNumeral;
+}
+
+List<int> getCodedInts(List<String> input) {
+  var wordToNumeral = getWordToNumeralMap();
+  List<int> codedInts = [];
+  for (String line in input) {
+    final matches =
+        regex.allMatches(line).map((match) => match.group(0)).toList();
+    if (matches != []) {
+      String first = matches[0] ?? "";
+      String last = matches[matches.length - 1] ?? "";
+      first = first.length > 1 ? wordToNumeral[first]! : first;
+      last = last.length > 1 ? wordToNumeral[last]! : last;
+      codedInts.add(int.parse(first + last));
+    }
+  }
+  return codedInts;
 }
