5ead434456f48ab8c1a72cd77d70231cbc9224e0
Wesley Cheek <66931618+wcheek@users.noreply.github.com>
2023-12-26 15:27:57 +0900

    Parsing the game input

--- day_2/lib/day_2.dart
+++ day_2/lib/day_2.dart
@@ -1,15 +1,66 @@
 class Game {
-  int? _id;
   bool? _possible;
+  (int, List<(String, int)>)? _gameInput;
+
   Game(String gameInfo) {
-    _id = 0;
     _possible = false;
+    _gameInput = parseGameInfo(gameInfo);
   }
-  int get id => _id!;
+
+  int get id => _gameInput!.$1;
   bool get possible => _possible!;
-  static (int, List<(String, int)>) parseGameInfo(gameInfo) {
+  (int, List<(String, int)>) get game => _gameInput!;
+
+  static (int, List<(String, int)>) parseGameInfo(String gameInfo) {
     /// Outputs a record of (gameID, [(color, numCubes)]
-    throw Error();
+    // Eg: Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+    // (1, [("red", 4), ("green", 6), ("blue", 9)])
+    List<String> gameInfoPieces = getGameInfoPieces(gameInfo);
+    int id = int.parse(gameInfoPieces[0].split(" ")[1]);
+    List<(String, int)> parsedGameInfoPieces =
+        parseGameInfoPieces(gameInfoPieces[1]);
+    return (id, parsedGameInfoPieces);
+  }
+
+  static List<String> getGameInfoPieces(String gameInfo) {
+    /// Return ["Game 1", "3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"]
+    return gameInfo.split(":").map((el) => el.trim()).toList();
+  }
+
+  static List<(String, int)> parseGameInfoPieces(String gameInfoColorNums) {
+    /// Input looks like "3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"
+    // I want [("red", 4), ("green", 6), ("blue", 9)]
+    int redScore = 0;
+    int greenScore = 0;
+    int blueScore = 0;
+    List<String> gameSets =
+        gameInfoColorNums.split(";").map((el) => el.trim()).toList();
+    // ["3 blue, 4 red", "1 red, 2 green, 6 blue", "2 green"]
+    for (String gameSet in gameSets) {
+      gameSet
+          // "3 blue, 4 red"
+          .split(",")
+          .map((el) => el.trim())
+          // ["3 blue", "4 red"]
+          .forEach((el) {
+        // "3 blue"
+        List<String> split = el.split(" ");
+        String color = split[1];
+        int score = int.parse(split[0]);
+        switch (color) {
+          case "red":
+            redScore = redScore + score;
+            break;
+          case "green":
+            greenScore = greenScore + score;
+            break;
+          case "blue":
+            blueScore = blueScore + score;
+            break;
+        }
+      });
+    }
+    return [("red", redScore), ("green", greenScore), ("blue", blueScore)];
   }
 }
 
--- day_2/test/day_2_test.dart
+++ day_2/test/day_2_test.dart
@@ -2,7 +2,16 @@ import 'package:day_2/day_2.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
+  String testString = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green";
+  test("Game Info pieces parsed currectly", () {
+    expect(Game.getGameInfoPieces(testString),
+        ["Game 1", "3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"]);
   });
+  final game = Game(testString);
+  test('Parse game input', () {
+    expect(game.game.$2, [("red", 5), ("green", 4), ("blue", 9)]);
+  }, skip: false);
+  test("Check ID", () {
+    expect(game.id, 1);
+  }, skip: false);
 }
