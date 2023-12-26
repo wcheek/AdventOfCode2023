85a6c7de4165bb2830388dc1ab9eaccb35cf5629
Wesley Cheek <66931618+wcheek@users.noreply.github.com>
2023-12-26 14:25:46 +0900

    creating game class

--- day_2/lib/day_2.dart
+++ day_2/lib/day_2.dart
@@ -1,3 +1,15 @@
-int calculate() {
-  return 6 * 7;
+class Game {
+  int? _id;
+  bool? _possible;
+  Game(String gameInfo) {
+    _id = 0;
+    _possible = false;
+  }
+  int get id => _id!;
+  bool get possible => _possible!;
+  static (int, List<(String, int)>) parseGameInfo(gameInfo) {
+    /// Outputs a record of (gameID, [(color, numCubes)]
+    throw Error();
+  }
 }
+
