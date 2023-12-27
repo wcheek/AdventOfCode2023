c08fbf5a40079ce37eb5a9c349d74d9e57e87e90
Wesley Cheek <66931618+wcheek@users.noreply.github.com>
2023-12-27 16:32:13 +0900

    Part one complete woo

--- day_3/bin/day_3.dart
+++ day_3/bin/day_3.dart
@@ -1,6 +1,7 @@
 import 'package:day_3/day_3.dart';
+import 'package:collection/collection.dart';
 
 void main(List<String> arguments) {
-  MachineSchema machineSchema = MachineSchema(filePath: "lib/testInput.txt");
-  print(machineSchema.numsAdjacent);
+  MachineSchema machineSchema = MachineSchema(filePath: "lib/input.txt");
+  print(machineSchema.numsAdjacent!.sum);
 }
