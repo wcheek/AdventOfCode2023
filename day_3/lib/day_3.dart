a69fb26b189b530e1626d948d7a8edceb57604d9
Wesley Cheek <66931618+wcheek@users.noreply.github.com>
2023-12-27 11:12:51 +0900

    day 3 line stuff

--- day_3/lib/day_3.dart
+++ day_3/lib/day_3.dart
@@ -1,3 +1,35 @@
-int calculate() {
-  return 6 * 7;
+import 'dart:io';
+
+List<String> getFileContents(String path) {
+  File input = File(path);
+  List<String> contents = input.readAsLinesSync();
+  return contents;
+}
+
+class LineInfo {
+  String? rawLine;
+  List<int>? numsInLine;
+
+  RegExp numRegex = RegExp(r"(\d+)");
+
+  LineInfo(String line) {
+    rawLine = line;
+    numsInLine = getNumsInLine(line);
+  }
+  List<int> getNumsInLine(String line) {
+    return numRegex
+        .allMatches(line)
+        .map((match) => int.parse(match.group(0)!))
+        .toList();
+  }
+}
+
+class MachineSchema {
+  MachineSchema({String filePath = "lib/testInput.txt"}) {
+    final fileInput = getFileContents(filePath);
+    List<LineInfo> lines = [];
+    for (String line in fileInput) {
+      lines.add(LineInfo(line));
+    }
+  }
 }
--- day_3/test/day_3_test.dart
+++ day_3/test/day_3_test.dart
@@ -1,8 +1,18 @@
 import 'package:day_3/day_3.dart';
 import 'package:test/test.dart';
 
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
+void testLineMethods() {
+  String testLine =
+      "...733.......289..262.....520..................161.462..........450.........................183.............................................";
+  LineInfo line = LineInfo(testLine);
+  test("Get nums in line", () {
+    expect(line.numsInLine, [733, 289, 262, 520, 161, 462, 458, 183]);
   });
+}
+
+void main() {
+  testLineMethods();
+  // test('Correct part numbers', () {
+  //   expect(getCorrectPartNumbers(), [467, 35, 633, 617, 592, 755, 664, 598]);
+  // }, skip: true);
 }
