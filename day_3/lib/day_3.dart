86dab3278d2af603b92e8ca34ae4b892786ec8e1
Wesley Cheek <66931618+wcheek@users.noreply.github.com>
2023-12-27 13:05:52 +0900

    Get symbols

--- day_3/lib/day_3.dart
+++ day_3/lib/day_3.dart
@@ -7,21 +7,35 @@ List<String> getFileContents(String path) {
 }
 
 class LineInfo {
-  String? rawLine;
+  int? lineNum;
   List<(int, List<int>)>? numsInLineInfo;
+  List<(String, int)>? symsInLineInfo;
 
   RegExp numRegex = RegExp(r"(\d+)");
+  RegExp symsRegex = RegExp(r"[\+\@\$\-\=\*\/\#\&\%]");
 
-  LineInfo(String line) {
-    rawLine = line;
+  LineInfo(String line, int lineNum) {
+    lineNum = lineNum;
     numsInLineInfo = getNumsInLine(line);
+    symsInLineInfo = getSymsInLine(line);
   }
+
   List<(int, List<int>)> getNumsInLine(String line) {
     var matches = numRegex.allMatches(line);
     var returnList = matches.map((match) {
       int num = int.parse(match.group(0)!);
       List<int> pos = [for (var i = match.start; i < match.end; i += 1) i];
       return (num, pos);
+    }).toList();
+    return returnList;
+  }
+
+  List<(String, int)> getSymsInLine(String line) {
+    var matches = symsRegex.allMatches(line);
+    var returnList = matches.map((match) {
+      String sym = match.group(0)!;
+      int pos = match.start;
+      return (sym, pos);
     }).toList();
     return returnList;
   }
@@ -32,9 +46,9 @@ class MachineSchema {
   MachineSchema({String filePath = "lib/testInput.txt"}) {
     final fileInput = getFileContents(filePath);
     _lines = [];
-    for (String line in fileInput) {
-      _lines?.add(LineInfo(line));
-    }
+    fileInput.asMap().forEach((ind, line) {
+      _lines?.add(LineInfo(line, ind));
+    });
   }
   List<LineInfo> get lines => _lines!;
 }
