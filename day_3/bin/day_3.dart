e431fef8e88634a6898336e10e509f3f3a62e1ac
Wesley Cheek <66931618+wcheek@users.noreply.github.com>
2023-12-27 10:24:17 +0900

    day 3 init

---
+++ day_3/bin/day_3.dart
@@ -0,0 +1,5 @@
+import 'package:day_3/day_3.dart' as day_3;
+
+void main(List<String> arguments) {
+  print('Hello world: ${day_3.calculate()}!');
+}
---
+++ day_3/test/day_3_test.dart
@@ -0,0 +1,8 @@
+import 'package:day_3/day_3.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
